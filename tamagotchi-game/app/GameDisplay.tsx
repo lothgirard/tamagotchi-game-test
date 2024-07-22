4a723be46d71bb6b362fd9de4887165853a4f75d
Léon Othenin-Girard <lgirard@student.ethz.ch>
2024-07-22 17:14:22 +0200

    react is the fucking worst

--- tamagotchi-game/app/GameDisplay.tsx
+++ tamagotchi-game/app/GameDisplay.tsx
@@ -1,12 +1,76 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 import { useActionList, useActionListDispatch, useProgress, useProgressDispatch, useGameState, useGameStateDispatch } from './GameState';
 import { stateCache } from 'expo-router/build/getLinkingConfig';
+import { waitUntilSymbol } from 'next/dist/server/web/spec-extension/fetch-event';
 
 
-export function GameDisplay({Styles}) {
-    var gameState = useGameState();
-    var gameStateDispatch = useGameStateDispatch();
+type Props = {
+    Styles: any,
+    GameState: any
+    setGameState: any
+}
+
+export function GameDisplay({Styles, GameState, setGameState}: Props) {
+    const gameState = useGameState();
+    const gameStateDispatch = useGameStateDispatch();
+
+
+    var output = 'assets/images/game-images/pets/' + gameState[0] + '.png';
+
+    // useEffect( () => {
+    //     if(gameState[0] === "hatchingAnim") {
+    //         var action = gameState;
+    //         action[0] = "hatched";
+    //         setTimeout(() => { console.log("timeout!"); gameStateDispatch(action)} , 0);
+    //     }
+    // }, [gameState, gameStateDispatch]
+    // );
+    //var output = "";
+
+    //useEffect(() => {
+    console.log("GameDisplay: " + GameState);
+    console.log(gameState);
+    switch(GameState[0]) {
+        case "hatched":
+        case "hatching":
+            output = 'assets/images/game-images/pets/' + gameState[0] + '.png';
+            break;
+        case "hatchingAnim":
+        case "test":
+            output = 'assets/images/game-images/pets/' + gameState[0] + '_broken.png';
+            console.log(output);
+        default: 
+    };
+    //}, [GameState, gameState]);
+                //console.log(gameState[0]);
+    //     var output = 'assets/images/game-images/pets/' + gameState[0] + '.png';
+    //     console.log(output);
+    //     return output;
+    // }
+
+    var gameplay = (image) => 
+        {
+            console.log("output is " + output);
+        return (
+        <View style={Styles.screenLayout}>
+            <View style={Styles.upperScreen}>
+                <Text style={Styles.screenText} adjustsFontSizeToFit={true}> 
+                    {screenPrompt(gameState)}
+                </Text>
+            </View>
+            <View style={Styles.lowerScreen}>
+                <Text style={Styles.leftText} adjustsFontSizeToFit={true}>
+                    {leftText(gameState)}
+                </Text>
+                <Image style={Styles.pet} source={{uri: image}}/>
+                <Text style={Styles.rightText} adjustsFontSizeToFit={true}>
+                    {rightText(gameState)}
+                </Text>
+            </View>
+        </View>
+        )
+    }
 
     switch(gameState[0]) {
         case 'stats':
@@ -16,13 +80,13 @@ export function GameDisplay({Styles}) {
         case 'credits':
             return credits(gameState);
         default: 
-            return ( gameplay(Styles, gameState) );
+            return gameplay(output);
     }
 }
 
 
 function screenPrompt(gameState: Array<String>) {
-    console.log(gameState[0]);
+    //console.log(gameState[0]);
     switch(gameState[0]) {
         case "hatching":
             return "Your egg looks ready to hatch! Are you ready?";
@@ -49,6 +113,22 @@ function rightText(gameState: Array<String>) {
     }
 }
 
+function timeHatchAnim(gameState: Array<String>, dispatch: any) {
+
+    
+    // useEffect(() => {
+    //     if(gameState[0] === "hatchingAnim") {
+    //         var action = gameState;
+    //         action[0] = "hatched";
+    //         console.log(action)
+    //         dispatch(action);
+    //         const timeOut = setTimeout(() => dispatch(action), 0);
+
+    //         return () => clearTimeout(timeOut);
+    //         }
+    //     }, [dispatch, gameState]);
+}
+
 function stats(gameState: Array<String>) {
     return (<View></View>);
 }
@@ -61,37 +141,34 @@ function credits(gameState: Array<String>) {
     return (<View></View>);
 }
 
-function gameplay(Styles, gameState: Array<String>) {
-    return (
-    <View style={Styles.screenLayout}>
-        <View style={Styles.upperScreen}>
-            <Text style={Styles.screenText}> 
-                {screenPrompt(gameState)}
-            </Text>
-        </View>
-        <View style={Styles.lowerScreen}>
-            <Text style={Styles.leftText}>
-                {leftText(gameState)}
-            </Text>
-            <Image style={Styles.pet} source={{uri: getImage(gameState)}}/>
-            <Text style={Styles.rightText}>
-                {rightText(gameState)}
-            </Text>
-        </View>
-    </View>
-    )
-}
-
-function getImage(gameState: Array<String>) {
+function getImage(gameState, gameStateDispatch) {
+    // const dispatchRef = useRef(gameStateDispatch);
+    // dispatchRef.current = gameStateDispatch;
     var output = 'assets/images/game-images/pets/' + gameState[0] + '.png';
-    console.log(gameState);
+    useEffect(() => {
+        if(gameState[0] === "hatchingAnim") {
+            var action = gameState;
+            action[0] = "hatched";
+            setTimeout(() => { console.log("timeout!"); gameStateDispatch(action)} , 0);
+        }}, [gameState, gameStateDispatch]
+    );
+    //console.log(gameState);
     switch(gameState[0]) {
+        case "hatched":
         case "hatching":
             output = 'assets/images/game-images/pets/' + gameState[1] + '.png';
+            return output; 
+        case "hatchingAnim":
+            output = 'assets/images/game-images/pets/' + gameState[1] + '_broken.png';
+            //console.log();
+            return output; 
         default: 
+            //console.log(gameState[0]);
             return output;
     }
     var output = 'assets/images/game-images/pets/' + gameState[0] + '.png';
     console.log(output);
     return output;
 }
+
+
--- tamagotchi-game/app/ScreenArea.tsx
+++ tamagotchi-game/app/ScreenArea.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { ImageBackground, Text, View, Image, Pressable } from 'react-native';
-import { useActionList, useActionListDispatch, useProgress, useProgressDispatch, useGameState, useGameStateDispatch } from './GameState';
+import { useActionList, useActionListDispatch, useProgress, useProgressDispatch, useGameState, useGameStateDispatch, GameStateContext, GameStateDispatchContext } from './GameState';
 import { GameDisplay } from './GameDisplay'; 
 
-export function ScreenArea({Styles}) {
-    var gameState = useGameState();
-    var gameStateDispatch = useGameStateDispatch();
+type Props = {
+    Styles: any,
+    GameState: any
+    setGameState: any
+}
 
+export function ScreenArea({Styles, GameState, setGameState}: Props) {
+    var gameState = useContext(GameStateContext);
+    var gameStateDispatch = useContext(GameStateDispatchContext);
+
+    console.log("ScreenArea: " + GameState);
+
+
+    useEffect(() => { 
+        if(gameState[0] === "hatchingAnim") {
+            console.log("bleep");
+        } else {
+            console.log("bloop");
+        }
+    }, [gameState]);
+    //console.log(GameState)
     return (
         <View style={Styles.upperDisplay}> 
             <Pressable onPress={() => leftButton(gameState, gameStateDispatch)}>
@@ -14,7 +31,7 @@ export function ScreenArea({Styles}) {
             </Pressable>
             <Pressable>
                 <ImageBackground source={{uri: "assets/images/game-images/window.png"}} style={ Styles.screen } resizeMode='contain'>
-                    <GameDisplay Styles={Styles}/>
+                    <GameDisplay Styles={Styles} GameState={GameState} setGameState={setGameState}/>
                 </ImageBackground>
             </Pressable>
             <Pressable onPress={() => rightButton(gameState, gameStateDispatch)}>
@@ -29,14 +46,14 @@ function leftButton(gameState: Array<String>, dispatch: any) {
     var action = gameState;
     switch(gameState[0]) {
         case "hatching":
-            action[0] = "hatched";
+            action[0] = "hatchingAnim";
         default:
             dispatch(action);
     }
 }
 
 function rightButton(gameState: Array<String>, dispatch: any) {
-    console.log("right!");
+    //console.log("right!");
     var action = gameState;
     switch(gameState[0]) {
         case "hatching":
@@ -47,7 +64,7 @@ function rightButton(gameState: Array<String>, dispatch: any) {
 }
 
 function selectOption(gameState: Array<String>, dispatch: any) {
-    console.log("select!");
+    //console.log("select!");
     var action = "";
     switch(gameState[0]) {
         default:
